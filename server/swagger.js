8420a0504283952fc4fa923439f435a62c1e7389
Hanna <100560594+Han-nahh@users.noreply.github.com>
2024-12-14 16:07:16 +0300

    Update swagger.js

--- server/swagger.js
+++ server/swagger.js
@@ -9,7 +9,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:5000',
+      url: 'https://vehicle-management-system-be.onrender.com',
       description: 'Local server',
     },
   ],
